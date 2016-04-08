7b4139f5e5fb240e08bca5ddb060fe2269472065
Mobilszoftver laboratórium <mobsoft@aut.bme.hu>
2016-04-08 09:03:46 +0200

    created ui package

--- app/src/main/java/hu/tomkom/deliveryapp/ui/MainActivity.java
+++ app/src/main/java/hu/tomkom/deliveryapp/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package hu.tomkom.deliveryapp;
+package hu.tomkom.deliveryapp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import hu.tomkom.deliveryapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
